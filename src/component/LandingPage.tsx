e34d83fe8be755650ec04d8fc8532c50f39f67ab
Victor <emmyvic98@gmail.com>
2024-02-22 13:04:11 +0100

    landing page section

--- src/component/LandingPage.tsx
+++ src/component/LandingPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Colors } from "@/styles";
+import { Colors, SCREENS } from "@/styles";
 import React from "react";
 import styled from "styled-components";
 import Image from "next/image";
@@ -10,7 +10,13 @@ export const LandingPage = () => {
     <MainPage>
       <Navbar>
         <a href="#" className="logo">
-          <Image src="/assets/logo.png" width={50} height={50} alt="Logo" style={{marginTop: '4px'}}/>
+          <Image
+            src="/assets/logo.png"
+            width={50}
+            height={50}
+            alt="Logo"
+            style={{ marginTop: "4px" }}
+          />
         </a>
         <div className="menu">
           <a href="#" className="menu__item">
@@ -23,11 +29,18 @@ export const LandingPage = () => {
       </Navbar>
       <HeroSection>
         <Slogan>
-            Shop, Sparkle and Shine
+          Shop, <br />
+          Sparkle
+          <br /> and Shine
         </Slogan>
-        <PreviewFrame>
-          <PictureSlide></PictureSlide>
-        </PreviewFrame>
+        <PreviewFrameContainer>
+          <PreviewFrame>
+            <PictureSlide $background="/assets/frame_slide_2.jpg"></PictureSlide>
+          </PreviewFrame>
+          <PreviewFrame>
+            <PictureSlide $background="/assets/frame_slide_3.jpg"></PictureSlide>
+          </PreviewFrame>
+        </PreviewFrameContainer>
       </HeroSection>
     </MainPage>
   );
@@ -65,29 +78,63 @@ const Navbar = styled.nav`
   }
 `;
 const HeroSection = styled.section`
-padding: 18px;
-padding-top: 12px;
+  padding: 18px;
+  padding-top: 12px;
+  max-width: 1200px;
+  margin: auto;
+  ${SCREENS.sm} {
+    display: flex;
+    justify-content: center;
+  }
+  ${SCREENS.lg} {
+    gap: 24px;
+  }
 `;
 const PreviewFrame = styled.div`
   width: 230px;
   padding: 2px;
   height: 300px;
   background: #fff;
-  margin: auto;
+  //   margin: auto;
   border-radius: 12px;
+
+  ${SCREENS.sm} {
+    margin: 0;
+    margin-top: 40px;
+    margin-left: 8px;
+  }
+  ${SCREENS.lg} {
+    width: 300px;
+    height: 391px;
+  }
+`;
+const PreviewFrameContainer = styled.div`
+  position: relative;
+  top: 0px;
+  width: 50%;
+  ${PreviewFrame}:nth-child(2) {
+    position: absolute;
+    top: 100px;
+    left: 125px;
+  }
 `;
 
-const PictureSlide = styled.div`
+const PictureSlide = styled.div<{$background: string}>`
   background-color: ${Colors.brown};
   background-position: center;
-  background-image: url(/assets/frame_slide_3.jpg);
+  background-image: url(${(props) => props.$background});
   height: 100%;
   background-size: cover;
   border-radius: 12px;
 `;
 const Slogan = styled.p`
-font-family: "Poppins", sans-serif;
-font-size: 3.6rem;
-color: #000;
-font-weight: 400;
-`
+  font-family: "Poppins", sans-serif;
+  font-size: 3.6rem;
+  color: #000;
+  font-weight: 400;
+
+  ${SCREENS.lg} {
+    font-size: 5rem;
+    margin-left: auto;
+  }
+`;
