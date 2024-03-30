87ff056a3eaa6c2cad1d8e05e22f660d675417d3
Victor <emmyvic98@gmail.com>
2024-03-30 17:41:33 +0100

    Homepage

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -1,4 +1,5 @@
 import { CustomInput } from "@/components/Form";
+import HorizontalNavigation from "@/components/HorizontalNavigation";
 import { ProductItemCard } from "@/components/ProductItemCard";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
@@ -7,9 +8,10 @@ export const HomePage = () => {
   return (
     <div>
       <ProductItemCard />
-      <div className="p-4" style={{ background: "#fff" }}>
+      <div className="p-4 mb-8" style={{ background: "#fff" }}>
         <CustomInput icon={faSearch} placeholder={"Search"} />
       </div>
+      <HorizontalNavigation />
     </div>
   );
 };
