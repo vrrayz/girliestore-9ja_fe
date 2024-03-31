dedb0a101d063814339528f6fb706dc96aacdf5d
Victor <emmyvic98@gmail.com>
2024-03-31 16:07:01 +0100

    header top logo and search transition

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -3,16 +3,44 @@ import { Header } from "@/components/Header";
 import HorizontalNavigation from "@/components/HorizontalNavigation";
 import { ProductItemCard } from "@/components/ProductItemCard";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 export const HomePage = () => {
+  const [headerTop, setHeaderTop] = useState<number>(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log(window.scrollY);
+      if (window.scrollY > 0 && window.scrollY <= 50) {
+        setHeaderTop(window.scrollY);
+      }
+      if (window.scrollY > 50) setHeaderTop(50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div>
-      <Header />
-      <div className="p-4 mb-8" style={{ background: "#fff" }}>
-      <HorizontalNavigation />
-      </div>
-      <ProductItemCard />
-    </div>
+    <Body>
+      <Header headerTop={headerTop} />
+      <div></div>
+      <section style={{ width: "100vw" }}>
+        <div className="p-4 mb-8" style={{ background: "#fff" }}>
+          <HorizontalNavigation />
+        </div>
+        <ProductItemCard />
+        <ProductItemCard />
+        <ProductItemCard />
+        <ProductItemCard />
+      </section>
+    </Body>
   );
 };
+
+const Body = styled.div`
+  display: grid;
+  grid-template-rows: 120px 1fr;
+`;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,35 +5,61 @@ import Image from "next/image";
 import { CustomInput } from "./Form";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-export const Header = () => {
+interface Props {
+  headerTop: number;
+}
+
+export const Header = ({ headerTop }: Props) => {
   return (
-    <HeaderContainer>
+    <HeaderContainer $headerTop={headerTop}>
       {/* Toggler Would be fixed at the top left */}
-      <div style={{ position: "fixed", top: "0px", left: "0px" }}>Toggler</div>
+      <div style={{ position: "fixed", top: "0px", left: "0px" }}>
+      </div>
       {/* Logo and search */}
-      <Image
-        src="/assets/logo.png"
-        width={50}
-        height={50}
-        alt="Logo"
-        className="mx-auto mb-3"
-      />
-      <CustomInput
-        icon={faSearch}
-        placeholder={"Search"}
-        className="w-11/12 mx-auto"
-      />
+      <LogoAndSearchContainer>
+        <Image
+          src="/assets/logo.png"
+          width={50}
+          height={50}
+          alt="Logo"
+          className="mx-auto mb-3 logo"
+        />
+        <CustomInput
+          icon={faSearch}
+          placeholder={"Search"}
+          className="w-full mx-auto header-input"
+        />
+      </LogoAndSearchContainer>
       {/* User Dropdown would be fixed at the top right */}
       <div style={{ position: "fixed", top: "0px", right: "0px" }}>User</div>
     </HeaderContainer>
   );
 };
-const HeaderContainer = styled.header`
-  position: relative;
+const HeaderContainer = styled.header<{ $headerTop: number }>`
+  position: fixed;
   display: flex;
   justify-content: center;
   flex-direction: column;
   background: ${Colors.white};
   box-shadow: #00000059 0px -1px 4px 1px;
   padding: 12px 16px;
+  width: 100vw;
+  top: ${(props) => 0 - props.$headerTop}px;
+//   transition: top 0ms;
+
+  .logo {
+    opacity: ${(props) => 1 - (props.$headerTop / 100) * 2};
+    // transition: opacity 0ms;
+  }
+
+  div.header-input{
+    width: ${(props) => 100 - (props.$headerTop / 2)}%;
+  }
+`;
+const LogoAndSearchContainer = styled.div`
+  position: relative;
+  top: 0px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 `;
