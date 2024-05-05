4c1fc70481af492d4b9f8be2efdd9337cbd3b013
Victor <emmyvic98@gmail.com>
2024-05-05 16:55:19 +0100

    dynamic categories

--- src/components/HorizontalNavigation.tsx
+++ src/components/HorizontalNavigation.tsx
@@ -1,23 +1,23 @@
+import { useCategories } from "@/hooks/useCategories";
 import { Colors } from "@/styles";
 import React from "react";
 import styled from "styled-components";
 
 const HorizontalNavigation = () => {
+  const { categories } = useCategories();
   return (
     <OverflowContainer className="no-scrollbar">
       <NavigationContainer>
-        <NavItem className="active">All</NavItem>
-        <NavItem>Arts &amp; Crafts</NavItem>
-        <NavItem>Furniture</NavItem>
-        <NavItem>Consumer Electronics</NavItem>
-        <NavItem>Light Industry &amp; Daily Use</NavItem>
+        {categories.map((item, i) => (
+          <NavItem key={i}>{item}</NavItem>
+        ))}
       </NavigationContainer>
     </OverflowContainer>
   );
 };
 const OverflowContainer = styled.div`
-overflow-x: scroll;
-`
+  overflow-x: scroll;
+`;
 const NavigationContainer = styled.div`
   display: flex;
   width: max-content;
@@ -25,12 +25,12 @@ const NavigationContainer = styled.div`
   margin: 0px 8px;
 `;
 const NavItem = styled.a`
-padding: 2px 8px;
-font-size: 0.95rem;
+  padding: 2px 8px;
+  font-size: 0.95rem;
 
-&.active{
+  &.active {
     border-radius: 2px;
     border-bottom: 4px solid ${Colors.red};
-}
+  }
 `;
 export default HorizontalNavigation;
