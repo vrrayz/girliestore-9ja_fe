6c6017c0bb233ec9053058a9cadbaaf146f62037
Victor <emmyvic98@gmail.com>
2024-06-08 14:13:07 +0100

    view products in homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,8 +4,12 @@ import HorizontalNavigation from "@/components/HorizontalNavigation";
 import React, { useEffect, useState } from "react";
 import { Body } from "./Styled";
 import { Header } from "./Header";
+import { useProducts } from "@/hooks/useProducts";
+import { Loading } from "./Loading";
+import { ProductItemCard, ProductListContainer } from "./ProductItemCard";
 
 export const HomePage = () => {
+  const { products, setProducts, isLoading } = useProducts();
   return (
     <Body>
       <Header displaySearch={true} />
@@ -14,10 +18,12 @@ export const HomePage = () => {
         <div className="p-4 mb-8" style={{ background: "#fff" }}>
           <HorizontalNavigation />
         </div>
-        {/* <ProductItemCard />
-        <ProductItemCard />
-        <ProductItemCard />
-        <ProductItemCard /> */}
+        {isLoading && <Loading />}
+        <ProductListContainer>
+          {products?.map((product, i) => (
+            <ProductItemCard product={product} key={i} />
+          ))}
+        </ProductListContainer>
       </section>
     </Body>
   );
