fe1759902170ac90532e5671fcf592957d6bc51b
Victor <emmyvic98@gmail.com>
2024-06-09 09:17:23 +0100

    minor cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,6 @@ import { HomePage } from "@/components/HomePage";
 import { InputField } from "@/components/Input";
 import { Loading } from "@/components/Loading";
 // import { LandingPage } from '@/component/LandingPage';
-import dynamic from "next/dynamic";
-
-const LandingPage = dynamic(() => import("@/components/LandingPage"), {
-  ssr: false,
-});
 
 export default function Home() {
   // return <LandingPage />;
