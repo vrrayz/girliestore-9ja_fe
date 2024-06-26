6eeb2900de94880f29c30b67374517aec71b2281
Victor <emmyvic98@gmail.com>
2024-06-26 12:27:56 +0100

    product layout progress

--- src/components/Products/Product.tsx
+++ src/components/Products/Product.tsx
@@ -1,42 +1,80 @@
 "use client";
 
-import { paramsToId } from "@/helpers";
+import { CURRENCY, paramsToId } from "@/helpers";
 import { useProduct } from "@/hooks/useProduct";
 import React from "react";
 import { Loading } from "../Loading";
 import Image from "next/image";
 import styled from "styled-components";
-import { CardBody, CardBodyHeadingOne, CardBodyText, CardContainer } from "../Card";
+import {
+  CardBody,
+  CardBodyHeadingOne,
+  CardBodyText,
+  CardContainer,
+} from "../Card";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretSquareRight } from "@fortawesome/free-regular-svg-icons/faCaretSquareRight";
+import {
+  InlineTextSmall,
+  InlineTextXl,
+  MultiplePhotosContainer,
+} from "../Styled";
+import { faStar, faTags } from "@fortawesome/free-solid-svg-icons";
+import { Colors } from "@/styles";
 
 interface Props {
   id: string;
 }
 export const Product = ({ id }: Props) => {
   const { product, isLoading, setIsLoading } = useProduct(paramsToId(id));
+  const starRatings = new Array(5).fill(0);
   return (
     <section style={{ width: "100vw" }}>
       {isLoading && <Loading />}
       {product && (
         <>
-          <Image
-            src={product.photos[0]?.url || "/assets/icons/default_product.png"}
-            width={200}
-            height={200}
-            alt="product_image"
-            className="mx-auto my-4"
-          />
+          <CustomMultiPhoto
+            $photoCount={product.photos.length}
+            $maxWidth={380}
+            $photoWidth={350}
+          >
+            {product.photos.map((photo, i) => (
+              <ProductImage
+                src={photo.url || "/assets/icons/default_product.png"}
+                width={200}
+                height={200}
+                alt="product_image"
+                className="mx-auto my-4"
+                key={i}
+              />
+            ))}
+          </CustomMultiPhoto>
           <ProductInfoContainer>
             <CardBody>
               <CardBodyHeadingOne>{product.name}</CardBodyHeadingOne>
               <CardBodyText>
-                {/* <FontAwesomeIcon icon={faCaretSquareRight} /> */}
-                <span>{product.category.name}</span>
+                <InlineTextSmall>
+                  Category:{" "}
+                  <a href="#" className="text-red font-bold">
+                    {product.category.name}
+                  </a>
+                </InlineTextSmall>
               </CardBodyText>
               <CardBodyText>
-                {/* <FontAwesomeIcon icon={faInfoCircle} />
-                <span>{store.description}</span> */}
+                <FontAwesomeIcon icon={faTags} />
+                <InlineTextXl className="font-extrabold">
+                  {CURRENCY} {product.price}
+                </InlineTextXl>
+              </CardBodyText>
+              <CardBodyText className="mb-2">
+                <InlineTextSmall>{product.quantity} items left</InlineTextSmall>
+              </CardBodyText>
+              <CardBodyText className="mb-2">
+                <StarRatingsContainer>
+                  {starRatings.map((rating, i) => (
+                    <FontAwesomeIcon icon={faStar} color={Colors.red} key={i} />
+                  ))}
+                </StarRatingsContainer>
               </CardBodyText>
             </CardBody>
           </ProductInfoContainer>
@@ -68,8 +106,6 @@ const ProductInfoContainer = styled(CardContainer)`
   ${CardBodyText} {
     display: flex;
     gap: 12px;
-    margin: 12px 0px;
-
     svg {
       margin: auto 0px;
     }
@@ -78,4 +114,19 @@ const ProductInfoContainer = styled(CardContainer)`
       margin: auto 0px;
     }
   }
+`;
+const ProductImage = styled(Image)`
+  width: 100%;
+  max-width: 350px;
+  height: 250px;
+  object-fit: cover;
+`;
+
+const CustomMultiPhoto = styled(MultiplePhotosContainer)`
+  gap: 12px;
+`;
+
+const StarRatingsContainer = styled.div`
+  display: flex;
+  gap: 2px;
 `;
