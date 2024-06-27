615c6077550ba935dd14da33ad09d1becd478c56
Victor <emmyvic98@gmail.com>
2024-06-27 11:59:58 +0100

    new type for sorting fetch queries

--- src/actions/apiRequest.ts
+++ src/actions/apiRequest.ts
@@ -39,14 +39,17 @@ export async function postDataUpload(
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
-export async function getData(url = "") {
-  const response = await fetch(`${process.env.BE_API_URL}${url}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: `Bearer ${cookies().get("access_token")?.value}`,
-    },
-  });
+export async function getData(url = "", data = {}) {
+  const response = await fetch(
+    `${process.env.BE_API_URL}${url}?${new URLSearchParams(data)}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${cookies().get("access_token")?.value}`,
+      },
+    }
+  );
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
