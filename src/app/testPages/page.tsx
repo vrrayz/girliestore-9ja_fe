cde3423501521488276988541af2acc80a930106
Victor <emmyvic98@gmail.com>
2024-07-02 22:12:38 +0100

    toast component

--- src/app/testPages/page.tsx
+++ src/app/testPages/page.tsx
@@ -1,103 +1,106 @@
 "use client";
 
+import { Toast } from "@/components/Toast";
+
 export default function Home() {
   return (
-    <div className="flex">
-      <div className="w-1/2">
-        <a href="#" className="styled-button my-5 ml-5">
-          Hello
-        </a>
-        <a href="#" className="styled-button radius my-5 ml-5">
-          Hello
-        </a>
-        <a href="#" className="styled-button dark my-5 ml-5">
-          Hello
-        </a>
-        <a href="#" className="styled-button dark radius my-5 ml-5">
-          Hello
-        </a>
-        <a href="#" className="styled-button gradient-slateblue my-5 ml-5">
-          Hello
-        </a>
-        <a
-          href="#"
-          className="styled-button gradient-slateblue radius my-5 ml-5"
-        >
-          Hello
-        </a>
-        <a
-          href="#"
-          className="styled-button gradient-slateblue__inverse my-5 ml-5"
-        >
-          Hello
-        </a>
-        <a
-          href="#"
-          className="styled-button gradient-slateblue__inverse radius my-5 ml-5"
-        >
-          Hello
-        </a>
-        <a
-          href="#"
-          className="styled-button gradient-olivedrab__inverse radius my-5 ml-5"
-        >
-          Hello
-        </a>
-        <a
-          href="#"
-          className="styled-button gradient-olivedrab__inverse my-5 ml-5"
-        >
-          Hello
-        </a>
-      </div>
-      <div className="w-1/2">
-        <a href="#" className="styled-button gradient-coral my-5 ml-5">
-          Hello
-        </a>
-        <a href="#" className="styled-button gradient-coral radius my-5 ml-5">
-          Hello
-        </a>
+    // <div className="flex">
+    //   <div className="w-1/2">
+    //     <a href="#" className="styled-button my-5 ml-5">
+    //       Hello
+    //     </a>
+    //     <a href="#" className="styled-button radius my-5 ml-5">
+    //       Hello
+    //     </a>
+    //     <a href="#" className="styled-button dark my-5 ml-5">
+    //       Hello
+    //     </a>
+    //     <a href="#" className="styled-button dark radius my-5 ml-5">
+    //       Hello
+    //     </a>
+    //     <a href="#" className="styled-button gradient-slateblue my-5 ml-5">
+    //       Hello
+    //     </a>
+    //     <a
+    //       href="#"
+    //       className="styled-button gradient-slateblue radius my-5 ml-5"
+    //     >
+    //       Hello
+    //     </a>
+    //     <a
+    //       href="#"
+    //       className="styled-button gradient-slateblue__inverse my-5 ml-5"
+    //     >
+    //       Hello
+    //     </a>
+    //     <a
+    //       href="#"
+    //       className="styled-button gradient-slateblue__inverse radius my-5 ml-5"
+    //     >
+    //       Hello
+    //     </a>
+    //     <a
+    //       href="#"
+    //       className="styled-button gradient-olivedrab__inverse radius my-5 ml-5"
+    //     >
+    //       Hello
+    //     </a>
+    //     <a
+    //       href="#"
+    //       className="styled-button gradient-olivedrab__inverse my-5 ml-5"
+    //     >
+    //       Hello
+    //     </a>
+    //   </div>
+    //   <div className="w-1/2">
+    //     <a href="#" className="styled-button gradient-coral my-5 ml-5">
+    //       Hello
+    //     </a>
+    //     <a href="#" className="styled-button gradient-coral radius my-5 ml-5">
+    //       Hello
+    //     </a>
 
-        <a
-          href="#"
-          className="styled-button gradient-coral__inverse radius my-5 ml-5"
-        >
-          Hello
-        </a>
-        <a href="#" className="styled-button gradient-coral__inverse my-5 ml-5">
-          Hello
-        </a>
+    //     <a
+    //       href="#"
+    //       className="styled-button gradient-coral__inverse radius my-5 ml-5"
+    //     >
+    //       Hello
+    //     </a>
+    //     <a href="#" className="styled-button gradient-coral__inverse my-5 ml-5">
+    //       Hello
+    //     </a>
 
-        <a href="#" className="styled-button gradient-tomato my-5 ml-5">
-          Hello
-        </a>
-        <a href="#" className="styled-button gradient-tomato radius my-5 ml-5">
-          Hello
-        </a>
+    //     <a href="#" className="styled-button gradient-tomato my-5 ml-5">
+    //       Hello
+    //     </a>
+    //     <a href="#" className="styled-button gradient-tomato radius my-5 ml-5">
+    //       Hello
+    //     </a>
 
-        <a
-          href="#"
-          className="styled-button gradient-tomato__inverse radius my-5 ml-5"
-        >
-          Hello
-        </a>
-        <a
-          href="#"
-          className="styled-button gradient-tomato__inverse my-5 ml-5"
-        >
-          Hello
-        </a>
+    //     <a
+    //       href="#"
+    //       className="styled-button gradient-tomato__inverse radius my-5 ml-5"
+    //     >
+    //       Hello
+    //     </a>
+    //     <a
+    //       href="#"
+    //       className="styled-button gradient-tomato__inverse my-5 ml-5"
+    //     >
+    //       Hello
+    //     </a>
 
-        <a
-          href="#"
-          className="styled-button gradient-olivedrab radius my-5 ml-5"
-        >
-          Hello
-        </a>
-        <a href="#" className="styled-button gradient-olivedrab my-5 ml-5">
-          Hello
-        </a>
-      </div>
-    </div>
+    //     <a
+    //       href="#"
+    //       className="styled-button gradient-olivedrab radius my-5 ml-5"
+    //     >
+    //       Hello
+    //     </a>
+    //     <a href="#" className="styled-button gradient-olivedrab my-5 ml-5">
+    //       Hello
+    //     </a>
+    //   </div>
+    // </div>
+    <Toast type="success" />
   );
 }
---
+++ src/components/Toast.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import { Colors } from "@/styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+
+type ToastType = "info" | "warning" | "failure" | "success";
+
+interface Props {
+  type?: ToastType;
+}
+
+export const Toast = ({ type }: Props) => {
+  return (
+    <>
+      <div className={`styled-toast ${type}`}>
+        {type && (
+          <Image
+            src={`/assets/icons/toast_${type}.svg`}
+            width={70}
+            height={70}
+            alt="toast_img"
+            className="toastImage"
+          />
+        )}
+        <button className="closeButton">
+          <FontAwesomeIcon icon={faClose} size="xl" />
+        </button>
+        <h3 className="text-xl font-bold mb-1">Hello !</h3>
+        <p className="message mt-1">
+          You successfully read this important message
+        </p>
+        <Image
+          src={"/assets/logo_bw.png"}
+          width={70}
+          height={70}
+          alt="toast_img"
+          className="toastLogo"
+        />
+      </div>
+    </>
+  );
+};
